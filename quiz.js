6edbdadee8967f6dfa9f1dd27bafa95c9b47e18f
Daniel Wambua <danielwambua09@outlook.com>
2024-10-07 10:13:40 +0300

    added javascript validation on the quiz app

---
+++ quiz.js
@@ -0,0 +1,19 @@
+function checkAnswer() {
+	var correctAnswer = "4";
+
+	let checkedRadio = document.querySelector('input[name="quiz"]:checked');
+
+	var userAnswer = checkedRadio.value;
+
+	if (userAnswer == correctAnswer) {
+		document.getElementById("feedback").innerHTML = "Correct Well done.";
+	}
+    
+    else {
+    	document.getElementById("feedback").innerHTML = "That's incorrect. Try again!";
+    }
+}
+
+var submitAnswer = document.getElementById("submit-answer");
+
+submitAnswer.addEventListener("click", checkAnswer);
